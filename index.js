bd56aea4a1770517227b8b3cd11eb052703ab6b0
Fahim <ashfahim18@gmail.com>
2025-12-22 23:15:17 +0600

    .env added and vercel

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const verifyFBToken = async(req,res,next) =>{
 }
 
 
-const uri = "mongodb+srv://Blood_Donation:xv0w08nWTu9igq64@cluster0.konzx.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.konzx.mongodb.net/?appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -48,7 +48,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const database = client.db('Blood_Donation')
         const dataCollection = database.collection('users')
@@ -63,12 +63,12 @@ async function run() {
              
         })
 
-        app.get('/users',verifyFBToken,async(req,res)=>{
+        app.get('/users',async(req,res)=>{
             const result = await dataCollection.find().toArray()
             res.send(result)
         })
 
-        app.get('/request',verifyFBToken,async(req,res)=>{
+        app.get('/request',async(req,res)=>{
             const result = await requestCollection.find().toArray()
             res.send(result)
         })
@@ -142,7 +142,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
